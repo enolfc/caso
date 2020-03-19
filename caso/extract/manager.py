enolfc/caso

commit 0288654ef5bffdbc3c6de0119e07dcf16aed6f69

Add granularity to last run and command line
Now the lastrun file is kept per project, and the projects can be
selected from command line, making easier the republication of single
projects, rather than the whole project list.

Fixes #9

caso/extract/manager.py
@@ -14,6 +14,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 import datetime
+import os.path
 
 import dateutil.parser
 from dateutil import tz
@@ -33,10 +34,6 @@ opts = [
     cfg.StrOpt('service_name',
                default='$site_name',
                help='Service name within the site'),
-    cfg.ListOpt('projects',
-                default=[],
-                deprecated_name='tenants',
-                help='List of projects to extract accounting records from.'),
     cfg.StrOpt('mapping_file',
                default='/etc/caso/voms.json',
                deprecated_group="extractor",
@@ -54,6 +51,10 @@ opts = [
 ]
 
 cli_opts = [
+    cfg.ListOpt('projects',
+                default=[],
+                deprecated_name='tenants',
+                help='List of projects to extract accounting records from.'),
     cfg.StrOpt('extract-to',
                deprecated_name='extract_to',
                help='Extract record changes until this date. '
@@ -92,47 +93,76 @@ class Manager(object):
         extractor_class = importutils.import_class(
             SUPPORTED_EXTRACTORS[CONF.extractor])
         self.extractor = extractor_class()
-        self.records = None
 
-    def _extract(self, extract_from, extract_to):
-        self.records = {}
-        for project in CONF.projects:
-            LOG.info("Extracting records for project '%s'" % project)
-            try:
-                records = self.extractor.extract_for_project(project,
-                                                             extract_from,
-                                                             extract_to)
-            except Exception:
-                records = []
-                LOG.exception("Cannot extract records for '%s'" % project)
-            else:
-                LOG.info("Extracted %d records for project '%s' from "
-                         "%s to %s" % (len(records), project, extract_from,
-                                       extract_to))
-            self.records.update(records)
+        self.last_run_base = os.path.join(CONF.spooldir, "lastrun")
 
-    def get_records(self, lastrun="1970-01-01"):
+    def get_lastrun(self, project):
+        lfile = "%s.%s" % (self.last_run_base, project)
+        if not os.path.exists(lfile):
+            LOG.warning("WARNING: Old global lastrun file detected and no "
+                        "project specific file found, using it for this run")
+            lfile = self.last_run_base
+
+        date = "1970-01-01"
+
+        if os.path.exists(lfile):
+            with open(lfile, "r") as fd:
+                date = fd.read()
+        else:
+            LOG.info("No lastrun file found, using '%s'" % date)
+        try:
+            date = dateutil.parser.parse(date)
+        except Exception:
+            LOG.error("ERROR: Could not read date from lastrun file '%s'" %
+                      lfile)
+            raise
+        else:
+            LOG.debug("Got '%s' from lastrun file '%s'" % (date, lfile))
+        return date
+
+    def write_lastrun(self, project):
+        if CONF.dry_run:
+            return
+        lfile = "%s.%s" % (self.last_run_base, project)
+        with open(lfile, "w") as fd:
+            fd.write(str(datetime.datetime.now(tz.tzutc())))
+
+    def get_records(self):
         """Get records from given date
-
-        :param lastrun: date to get records from (optional).
 
         If CONF.extract_from is present, it will be used instead of the
         lastrun parameter. If CONF.extract_to is present, it will be used
         instead of the extract_to parameter
         """
-        extract_from = CONF.extract_from or lastrun
         extract_to = CONF.extract_to or datetime.datetime.utcnow()
-
-        if isinstance(extract_from, six.string_types):
-            extract_from = dateutil.parser.parse(extract_from)
         if isinstance(extract_to, six.string_types):
             extract_to = dateutil.parser.parse(extract_to)
-
-        if extract_from.tzinfo is None:
-            extract_from.replace(tzinfo=tz.tzutc())
         if extract_to.tzinfo is None:
             extract_to.replace(tzinfo=tz.tzutc())
 
-        if self.records is None:
-            self._extract(extract_from, extract_to)
-        return self.records
+        records = {}
+        for project in CONF.projects:
+            LOG.info("Extracting records for project '%s'" % project)
+
+            extract_from = CONF.extract_from or self.get_lastrun(project)
+            if isinstance(extract_from, six.string_types):
+                extract_from = dateutil.parser.parse(extract_from)
+            if extract_from.tzinfo is None:
+                extract_from.replace(tzinfo=tz.tzutc())
+
+            LOG.debug("Extracting records from '%s'" % extract_from)
+            LOG.debug("Extracting records to '%s'" % extract_to)
+            try:
+                records = self.extractor.extract_for_project(project,
+                                                             extract_from,
+                                                             extract_to)
+            except Exception:
+                LOG.exception("Cannot extract records for '%s', got "
+                              "the following exception: " % project)
+            else:
+                LOG.info("Extracted %d records for project '%s' from "
+                         "%s to %s" % (len(records), project, extract_from,
+                                       extract_to))
+                records.update(records)
+                self.write_lastrun(project)
+        return records

caso/manager.py
@@ -14,12 +14,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import datetime
 import os
 import os.path
 
-import dateutil.parser
-from dateutil import tz
 from oslo_concurrency import lockutils
 from oslo_config import cfg
 
@@ -66,36 +63,18 @@ CONF.register_cli_opts(cli_opts)
 class Manager(object):
     def __init__(self):
         utils.makedirs(CONF.spooldir)
-        self.last_run_file = os.path.join(CONF.spooldir, "lastrun")
 
         self.extractor_manager = caso.extract.manager.Manager()
         self.messenger = caso.messenger.Manager()
 
         self.lock_path = CONF.lock_path
 
-    @property
-    def lastrun(self):
-        if os.path.exists(self.last_run_file):
-            with open(self.last_run_file, "r") as fd:
-                date = fd.read()
-        else:
-            date = "1970-01-01"
-
-        try:
-            date = dateutil.parser.parse(date)
-        except Exception:
-            # FIXME(aloga): raise a proper exception here
-            raise
-        return date
-
     def run(self):
         @lockutils.synchronized("caso_should_not_run_in_parallel",
                                 lock_path=self.lock_path, external=True)
         def synchronized():
-            records = self.extractor_manager.get_records(lastrun=self.lastrun)
+            records = self.extractor_manager.get_records()
             if not CONF.dry_run:
                 self.messenger.push_to_all(records)
-                with open(self.last_run_file, "w") as fd:
-                    fd.write(str(datetime.datetime.now(tz.tzutc())))
 
         return synchronized()

caso/tests/extract/test_manager.py
@@ -21,6 +21,7 @@ import uuid
 
 import dateutil.parser
 import mock
+import six
 
 from caso.extract import manager
 from caso.tests import base
@@ -47,22 +48,46 @@ class TestCasoManager(base.TestCase):
 
         with mock.patch.object(self.manager.extractor,
                                "extract_for_project") as m:
-            self.manager._extract("1999-12-19", "2015-10-23")
+            ret = self.manager.get_records()
             self.assertFalse(m.called)
-        self.assertEqual({}, self.manager.records)
+        self.assertEqual({}, ret)
 
     def test_extract(self):
         records = {uuid.uuid4().hex: None}
         self.flags(projects=["bazonk"])
+        extract_from = "1999-12-19"
+        extract_to = "2015-12-19"
+        self.flags(extract_from=extract_from)
+        self.flags(extract_to=extract_to)
 
-        with mock.patch.object(self.manager.extractor,
-                               "extract_for_project") as m:
+        if six.PY3:
+            builtins_open = 'builtins.open'
+        else:
+            builtins_open = '__builtin__.open'
+
+        with mock.patch(builtins_open) as fopen, \
+                mock.patch.object(self.manager.extractor,
+                                  "extract_for_project") as m:
+
+            aux = six.StringIO()
+            fopen.return_value.__enter__ = lambda x: aux
+            fopen.return_value.__exit__ = mock.Mock()
+
             m.return_value = records
-            self.manager._extract("1999-12-19", "2015-12-19")
-            m.assert_called_once_with("bazonk", "1999-12-19", "2015-12-19")
-        self.assertEqual(records, self.manager.records)
+            ret = self.manager.get_records()
+            m.assert_called_once_with(
+                "bazonk",
+                dateutil.parser.parse(extract_from),
+                dateutil.parser.parse(extract_to)
+            )
+            fopen.assert_called_once_with(
+                '/var/spool/caso/lastrun.bazonk',
+                'w'
+            )
+        self.assertEqual(records, ret)
 
     def test_get_records_wrong_extract_from(self):
+        self.flags(projects=["foo"])
         self.flags(extract_from="1999-12-99")
         self.assertRaises(ValueError,
                           self.manager.get_records)
@@ -72,31 +97,60 @@ class TestCasoManager(base.TestCase):
         self.assertRaises(ValueError,
                           self.manager.get_records)
 
-    def test_get_records_wrong_lastrun(self):
-        self.assertRaises(ValueError,
-                          self.manager.get_records,
-                          lastrun="1999-12-99")
-
-    def test_get_records_with_extract_from_and_to(self):
-        date = "1999-12-11"
-        end_date = "2015-12-11"
-        dt = dateutil.parser.parse(date)
-        parsed_end_date = dateutil.parser.parse(end_date)
-        self.flags(extract_from=date, extract_to=end_date)
-
-        with mock.patch.object(self.manager, "_extract") as m:
-            self.manager.get_records()
-            m.assert_called_with(dt, parsed_end_date)
-
     def test_get_records_with_lastrun(self):
-        date = "1999-12-11"
+        records = {uuid.uuid4().hex: None}
+        self.flags(dry_run=True)
+        self.flags(projects=["bazonk"])
+        lastrun = "1999-12-11"
+        extract_to = "2015-12-19"
+        self.flags(extract_to=extract_to)
 
-        dt = dateutil.parser.parse(date)
-        mock_now = datetime.datetime(2016, 4, 19, 15, 4, 14, 468060)
+        with mock.patch.object(self.manager.extractor,
+                               "extract_for_project") as m, \
+                mock.patch.object(self.manager, "get_lastrun") as m_lr:
 
-        with mock.patch.object(self.manager, "_extract") as m:
-            with mock.patch('caso.extract.manager.datetime') as MockDatetime:
-                MockDatetime.datetime.utcnow.return_value = mock_now
+            m_lr.return_value = lastrun
+            m.return_value = records
 
-                self.manager.get_records(lastrun=date)
-                m.assert_called_with(dt, mock_now)
+            ret = self.manager.get_records()
+
+            m_lr.assert_called_once_with("bazonk")
+            m.assert_called_once_with(
+                "bazonk",
+                dateutil.parser.parse(lastrun),
+                dateutil.parser.parse(extract_to)
+            )
+        self.assertEqual(records, ret)
+
+    def test_lastrun_exists(self):
+        expected = datetime.datetime(2014, 12, 10, 13, 10, 26, 664598)
+        aux = six.StringIO(str(expected))
+
+        if six.PY3:
+            builtins_open = 'builtins.open'
+        else:
+            builtins_open = '__builtin__.open'
+
+        with mock.patch("os.path.exists") as path, \
+                mock.patch(builtins_open) as fopen:
+            fopen.return_value.__enter__ = lambda x: aux
+            fopen.return_value.__exit__ = mock.Mock()
+            path.return_value = True
+
+            self.assertEqual(expected, self.manager.get_lastrun("foo"))
+
+    def test_lastrun_is_invalid(self):
+        aux = six.StringIO("foo")
+
+        if six.PY3:
+            builtins_open = 'builtins.open'
+        else:
+            builtins_open = '__builtin__.open'
+
+        with mock.patch("os.path.exists") as path:
+            with mock.patch(builtins_open) as fopen:
+                fopen.return_value.__enter__ = lambda x: aux
+                fopen.return_value.__exit__ = mock.Mock()
+                path.return_value = True
+
+                self.assertRaises(ValueError, self.manager.get_lastrun, "foo")

caso/tests/test_manager.py
@@ -16,9 +16,6 @@
 Tests for `caso.manager` module.
 """
 
-import datetime
-
-from dateutil import tz
 import mock
 from oslo_concurrency.fixture import lockutils as lock_fixture
 import six
@@ -51,57 +48,11 @@ class TestCasoManager(base.TestCase):
 
         super(TestCasoManager, self).tearDown()
 
-    def test_lastrun_does_not_exist(self):
-        self.manager.last_run_file = "foobarbarz/does/not/exist"
-        expected = datetime.datetime(1970, 1, 1, 0, 0)
-        self.assertEqual(expected, self.manager.lastrun)
-
-    def test_lastrun_exists(self):
-        expected = datetime.datetime(2014, 12, 10, 13, 10, 26, 664598)
-        aux = six.StringIO(str(expected))
-
-        if six.PY3:
-            builtins_open = 'builtins.open'
-        else:
-            builtins_open = '__builtin__.open'
-
-        with mock.patch("os.path.exists") as path:
-            with mock.patch(builtins_open) as fopen:
-                fopen.return_value.__enter__ = lambda x: aux
-                fopen.return_value.__exit__ = mock.Mock()
-                path.return_value = True
-
-                self.assertEqual(expected, self.manager.lastrun)
-
-    def test_lastrun_is_invalid(self):
-        aux = six.StringIO("foo")
-
-        # NOTE(aloga): manager.lastrun is a property, so we need to
-        # create our own callable here.
-        def call(self):
-            return self.manager.lastrun
-
-        if six.PY3:
-            builtins_open = 'builtins.open'
-        else:
-            builtins_open = '__builtin__.open'
-
-        with mock.patch("os.path.exists") as path:
-            with mock.patch(builtins_open) as fopen:
-                fopen.return_value.__enter__ = lambda x: aux
-                fopen.return_value.__exit__ = mock.Mock()
-                path.return_value = True
-
-                self.assertRaises(ValueError, call, self)
-
     def test_dry_run(self):
         self.flags(dry_run=True)
         # NOTE(aloga): cannot patch a property of an instance, see
         # https://code.google.com/p/mock/issues/detail?id=117
-        with mock.patch("caso.manager.Manager.lastrun",
-                        new_callable=mock.PropertyMock) as lastrun:
-            lastrun.return_value = datetime.datetime.now(tz.tzutc())
-            mngr = manager.Manager()
-            mngr.messenger.push_to_all.assert_not_called()
-            mngr.run()
-            self.assertFalse(mngr.messenger.push_to_all.called)
+        mngr = manager.Manager()
+        mngr.messenger.push_to_all.assert_not_called()
+        mngr.run()
+        self.assertFalse(mngr.messenger.push_to_all.called)

tox.ini
@@ -51,6 +51,6 @@ commands = oslo-config-generator --config-file=etc/caso/caso-config-generator.co
 # E123, E125 skipped as they are invalid PEP-8.
 
 show-source = True
-ignore = E123,E125,H803,H405
+ignore = E123,E125,H803,H405,H904
 builtins = _
 exclude=.venv,.git,.tox,dist,doc,*lib/python*,*egg,build

commit d2ac02ab6449a33495a6e94b7908060f4215698e

Fix bug that made cASO report only last project configured
The commit 0288654e introduced a bug in the way we collected all the
records, that made cASO report only for the last configured (i.e. the
last on the list) project.

Fix: #74

caso/extract/manager.py
@@ -66,7 +66,6 @@ class Manager(object):
     def __init__(self):
         extractor = loading.get_available_extractors()[CONF.extractor]
         self.extractor = extractor()
-        self.records = None
         self.last_run_base = os.path.join(CONF.spooldir, "lastrun")
 
     def get_lastrun(self, project):
@@ -113,7 +112,7 @@ class Manager(object):
         if extract_to.tzinfo is None:
             extract_to.replace(tzinfo=tz.tzutc())
 
-        records = {}
+        all_records = {}
         for project in CONF.projects:
             LOG.info("Extracting records for project '%s'" % project)
 
@@ -136,6 +135,6 @@ class Manager(object):
                 LOG.info("Extracted %d records for project '%s' from "
                          "%s to %s" % (len(records), project, extract_from,
                                        extract_to))
-                records.update(records)
+                all_records.update(records)
                 self.write_lastrun(project)
-        return records
+        return all_records