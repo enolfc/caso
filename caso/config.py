568b02178f37a47b4cf6dc654eef7e5f73574c47
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2024-09-27 11:21:37 +0200

    build: change build system to poetry

--- caso/config.py
+++ caso/config.py
@@ -35,6 +35,6 @@ def parse_args(argv, default_config_files=None):
     cfg.CONF(
         argv[1:],
         project="caso",
-        version=caso.__version__,
+        version=caso.extract_version(),
         default_config_files=default_config_files,
     )
